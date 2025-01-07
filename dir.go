f8895a45ddada60e1564dedfda91515d15bf2f71
Robert Hawk <robert.hawk@gmail.com>
2025-01-06 19:18:43 -0700

    feat: New directory utility

---
+++ dir.go
@@ -0,0 +1,69 @@
+package files
+
+import (
+	"io/fs"
+	"os"
+
+	"github.com/pkg/errors"
+	"github.com/rs/zerolog"
+)
+
+type Directory struct {
+	path           string
+	subdirectories []Directory
+	files          []File
+	logger         *zerolog.Logger
+}
+
+func NewDirectory(path string, logger *zerolog.Logger) (Directory, error) {
+	var sudbirectories []Directory
+	var files []File
+
+	fileSystem := os.DirFS(path)
+
+	err := fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		if d.IsDir() {
+			directory, err := NewDirectory(d.Name(), logger)
+			if err != nil {
+				return errors.WithStack(err)
+			}
+
+			sudbirectories = append(sudbirectories, directory)
+			return nil
+		}
+
+		file, err := NewFile(d.Name(), logger)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+
+		files = append(files, file)
+		return nil
+	})
+	if err != nil {
+		return Directory{}, err
+	}
+
+	return Directory{
+		path:           path,
+		subdirectories: sudbirectories,
+		files:          files,
+		logger:         logger,
+	}, nil
+}
+
+func (d Directory) Filter(ext string) []File {
+	var filtered []File
+
+	for _, file := range d.files {
+		if file.Ext() == ext {
+			filtered = append(filtered, file)
+		}
+	}
+
+	return filtered
+}
